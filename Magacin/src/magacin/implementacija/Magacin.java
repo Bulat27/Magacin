aaa9ffa7c4f59274b7f0b4ad40f45d77a18e7324
Bulat27 <nbulat99@gmail.com>
2022-03-14 17:25:42 +0100

    Dodata klasa Magacin koja implementira MagacinInterfejs (prazne metode)

---
+++ Magacin/src/magacin/implementacija/Magacin.java
@@ -0,0 +1,25 @@
+package magacin.implementacija;
+
+import artikli.Artikal;
+import magacin.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal vratiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}

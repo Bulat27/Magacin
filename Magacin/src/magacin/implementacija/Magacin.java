6098b784831e08ac4e7930d3e3245d4da518beb0
Bulat27 <nbulat99@gmail.com>
2022-03-14 17:36:33 +0100

    Dodata implementacija metode dodajArtikal metode Magacin

--- Magacin/src/magacin/implementacija/Magacin.java
+++ Magacin/src/magacin/implementacija/Magacin.java
@@ -1,14 +1,27 @@
 package magacin.implementacija;
 
+import java.util.List;
+
 import artikli.Artikal;
 import magacin.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
 
+	private List<Artikal> artikli;
+
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		boolean nasao = false;
 
+		for (Artikal a : artikli) {
+			if (a.equals(artikal)) {
+				a.setKolicina(a.getKolicina() + 1);
+				nasao = true;
+			}
+		}
+		
+		if (!nasao)
+			artikli.add(artikal);
 	}
 
 	@Override
